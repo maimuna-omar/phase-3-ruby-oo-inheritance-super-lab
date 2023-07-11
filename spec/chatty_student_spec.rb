d3ad887db4b51f73c619dce3b0e3b2f1d7ef256f
maimuna omar <maymunaabdi07@gmail.com>
2023-07-11 09:08:09 +0300

    done

--- spec/chatty_student_spec.rb
+++ spec/chatty_student_spec.rb
@@ -1,16 +1,19 @@
+require_relative '../lib/chatty_student'
+
 describe ChattyStudent do
   let!(:chatty_student) { ChattyStudent.new }
-  
+
   describe "#hello" do
     it "returns a greeting phrase plus a long chatty phrase" do
-      expected_output = "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+      expected_output = "Hey there! I'm so excited to learn stuff.\n" \
+                        "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died...\n"
       expect { chatty_student.hello }.to output(expected_output).to_stdout
     end
   end
 
-  describe "#raise_hand" do 
-    it "returns the phrase 'Pick me!' ten times." do
-      expected_output = "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+  describe "#raise_hand" do
+    it "returns the phrase 'Pick me!' ten times" do
+      expected_output = "Pick me!\n" * 10
       expect { chatty_student.raise_hand }.to output(expected_output).to_stdout
     end
   end
